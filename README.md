b70cc9c2f1d77cdd1f14b068cf52eb99644335ab
Timur Gusmanov <archrunel@gmail.com>
2022-07-27 10:25:35 +0600

    first publish to pypi

--- README.md
+++ README.md
@@ -1,14 +1,35 @@
 YandexCloud DNS Authenticator plugin for Certbot
 ================================================
 
+[yandexcloud](https://cloud.yandex.ru/) DNS Authenticator plugin for [certbot](https://certbot.eff.org/).
+
+This plugin automates the process of completing a dns-01 challenge by creating, and subsequently removing, TXT records using the yandexcloud [API](https://github.com/yandex-cloud/cloudapi)
+
 Installation
 ------------
-    pip install -e certbot-dns-yandexcloud/
+    pip install certbot-dns-yandexcloud
+
+Certbot Arguments
+---------------
+
+To start using DNS authentication for yandexcloud, pass the following arguments on certbot's command line:
+
+Option|Description|
+---|---|
+`--authenticator dns-yandexcloud`|select the authenticator plugin (Required)|
+`--dns-yandexcloud-credentials FILE_PATH`|yandexcloud credentials INI file path. (Required)|
 
 Credentials
 -----------
-    dns_yandexcloud_sa_json_path = "./sa.json"
+
+Before credentials.ini providing create [yandexcloud service account](https://cloud.yandex.ru/docs/iam/operations/sa/create) and create [service-account-key](https://cloud.yandex.ru/docs/cli/operations/authentication/service-account) and get your folder_id where DNS zone is created
+
+An example `credentials.ini` file:
+
+``` {.sourceCode .ini}
+    dns_yandexcloud_sa_json_path = "~/.secrets/certbot/sa.json"
     dns_yandexcloud_folder_id = "b0g0kfc0jutlhm000r00"
+```
 
 Examples
 --------
--- setup.py
+++ setup.py
@@ -1,18 +1,52 @@
 from setuptools import setup, find_packages
 
+version = '0.0.1'
+
+install_requires = [
+    'yandexcloud>=0.175.0',
+    'certbot>=1.29.0',
+]
+
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name='certbot-dns-yandexcloud',
-    version='0.0.1',
+    version=version,
     description='YandexCloud DNS Authenticator plugin for Certbot',
-    package='certbot_dns_yandexcloud.py',
-    install_requires=[
-        'yandexcloud'
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PykupeJIbc/certbot-dns-yandexcloud',
+    author='Timur Gusmanov',
+    author_email='archrunel@gmail.com',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3 :: Only",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
     ],
+    
     packages=find_packages(),
+    python_requires=">=3.8",
     include_package_data=True,
+    install_requires=install_requires,
     entry_points={
         'certbot.plugins': [
-            'dns-yandexcloud = certbot_dns_yandexcloud:Authenticator',
+            'dns-yandexcloud = certbot_dns_yandexcloud.certbot_dns_yandexcloud:Authenticator',
         ],
     },
 )
